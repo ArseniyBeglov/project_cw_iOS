75ddb0747676602d954b081c7d5abf820bc3ff55
ArseniyBeglov <HUDAIDOTT@yandex.ru>
2023-10-09 19:24:44 +0300

    added project files for classwork

---
+++ project_cw_ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  project_cw_ios
+//
+//  Created by Арсений Беглов on 09.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ project_cw_iosUITests/project_cw_iosUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  project_cw_iosUITestsLaunchTests.swift
+//  project_cw_iosUITests
+//
+//  Created by Арсений Беглов on 09.10.2023.
+//
+
+import XCTest
+
+final class project_cw_iosUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
